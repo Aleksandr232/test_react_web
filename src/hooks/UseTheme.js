004d91bf5c538701815342c29bbdc4477f0ce259
Aleksandr232 <aleksrowing232@gmail.com>
2024-02-26 01:30:12 +0300

    finish

--- src/hooks/UseTheme.js
+++ src/hooks/UseTheme.js
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 
 export const useTheme = () => {
     const [theme, setStoredTheme] = useState(() => {
-      // При первоначальной загрузке страницы получаем тему из localstorage
+
       const savedTheme = localStorage.getItem('theme');
       return savedTheme || 'light';
     });
   
     const setTheme = (newTheme) => {
-      // Устанавливаем новую тему
+      
       setStoredTheme(newTheme);
-      // Сохраняем новую тему в localstorage
+      
       localStorage.setItem('theme', newTheme);
     };
 
